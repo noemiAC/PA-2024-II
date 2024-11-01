fe5cd368c05978f21c16115ac07a009255a74009
noemiAC <168385917+noemiAC@users.noreply.github.com>
2024-11-01 12:56:13 -0500

    Update Code_block.py

--- CÓDIGO/Code_block.py
+++ CÓDIGO/Code_block.py
@@ -1,1 +1,15 @@
+import streamlit as st
+
+# Título de la aplicación
+st.title("Aplicación Simple con Streamlit")
+
+# Subtítulo o descripción
+st.write("Esta es una aplicación simple que saluda al usuario.")
+
+# Entrada de texto para el nombre del usuario
+nombre = st.text_input("Escribe tu nombre:")
+
+# Mostrar el saludo al presionar el botón
+if st.button("Saludar"):
+    st.write(f"¡Hola, {nombre}! Bienvenido a la aplicación de Streamlit.")
 
